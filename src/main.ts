83726c94e36bb8493cac5ed2b1bcf2699f4005f8
tama <tama@ttk1.net>
2019-11-15 01:44:31 +0900

    ejs導入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import express = require('express');
 
 const app = express();
-app.use(express.static('html'));
+app.set('view engine', 'ejs');
+app.set('view options', {delimiter: '?'})
+app.get('/', (req, res) => {
+  res.render('./index', { msg: 'hello, world!' });
+});
 app.listen(3000);
