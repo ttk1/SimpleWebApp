27c8414bb8a0f8e939baac097d44fcc0770a0188
tama <tama@ttk1.net>
2019-11-19 01:54:21 +0900

    署名の検証部分をミドルウェア化

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,19 @@
 import fs = require('fs');
 import express = require('express');
-import jws = require('jws');
+import { verify } from './verify';
 
 const conf = JSON.parse(String(fs.readFileSync('conf/conf.json')));
-const keys: { [key: string]: string } = JSON.parse(String(fs.readFileSync(conf.keys)));
-
-// https://cloud.google.com/iap/docs/signed-headers-howto?hl=ja
-function verify(signature: string): jws.Signature {
-  try {
-    const now = Date.now() / 1000;
-    const decoded = jws.decode(signature);
-
-    // 署名の検証
-
-    const alg = decoded?.header?.alg;
-    const kid = decoded?.header?.kid;
-    if (!(alg && kid && keys[kid] && jws.verify(signature, alg, keys[kid]))) {
-      console.log('invalid signature');
-      return null;
-    }
-
-    // ペイロードのチェック
-
-    // 有効期限のチェック
-    if (decoded.payload.exp < now) {
-      console.log('exp', decoded.payload.exp, now);
-      return null;
-    }
-    // 発行時のチェック
-    if (now < decoded.payload.iat) {
-      console.log('iat', now, decoded.payload.exp);
-      return null;
-    }
-    // 対象のチェック
-    if (decoded.payload.aud !== conf.aud) {
-      console.log('aud', decoded.payload.aud, conf.aud);
-      return null;
-    }
-    // 発行元のチェック
-    if (decoded.payload.iss !== conf.iss) {
-      console.log('iss', decoded.payload.iss, conf.iss);
-      return null;
-    }
-    return decoded;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-}
 
 const app = express();
 app.set('trust proxy', true);
 app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
+app.use(verify(conf));
 
 app.get('/*', (req, res) => {
-  const signature = req.header('x-goog-iap-jwt-assertion');
-  const decoded = verify(signature);
-  if (decoded) {
-    res.render('./index', {
-      msg: 'Welcome!'
-    });
-  } else {
-    res.render('./index', {
-      msg: 'Unauthorized!'
-    });
-  }
+  res.render('./index', {
+    msg: 'Welcome!'
+  });
 });
 
 app.listen(process.env.PORT || 3000);
---
+++ src/verify.ts
@@ -0,0 +1,46 @@
+import fs = require('fs');
+import jws = require('jws');
+import express = require('express');
+
+export function verify(conf: { [key: string]: string }): express.RequestHandler {
+  const keys: { [key: string]: string } = JSON.parse(String(fs.readFileSync(conf.keys)));
+  return (req, res, next): void => {
+    try {
+      const now = Date.now() / 1000;
+      const signature = req.header('x-goog-iap-jwt-assertion');
+      const decoded = jws.decode(signature);
+
+      // 署名の検証
+
+      const alg = decoded?.header?.alg;
+      const kid = decoded?.header?.kid;
+      if (!(alg && kid && keys[kid] && jws.verify(signature, alg, keys[kid]))) {
+        throw new Error('invalid signature');
+      }
+
+      // ペイロードのチェック
+
+      // 有効期限のチェック
+      if (decoded.payload.exp < now) {
+        throw new Error(`exp ${decoded.payload.exp} ${now}`);
+      }
+      // 発行時のチェック
+      if (now < decoded.payload.iat) {
+        throw new Error(`iat ${now} ${decoded.payload.exp}`);
+      }
+      // 対象のチェック
+      if (decoded.payload.aud !== conf.aud) {
+        throw new Error(`aud ${decoded.payload.aud} ${conf.aud}`);
+      }
+      // 発行元のチェック
+      if (decoded.payload.iss !== conf.iss) {
+        throw new Error(`iss ${decoded.payload.iss} ${conf.iss}`);
+      }
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(401).send('Unauthorized!');
+      return;
+    }
+  };
+}
