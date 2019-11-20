168006d75f10eb383dd85ba89dc3767995dfa4b6
tama <tama@ttk1.net>
2019-11-20 23:00:51 +0900

    firestoreに接続してみた。簡単でいいね。

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import fs = require('fs');
 import express = require('express');
+import { Firestore } from '@google-cloud/firestore';
 import { verify } from './verify';
 
 const conf = JSON.parse(String(fs.readFileSync('conf/conf.json')));
+const firestore = new Firestore();
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,9 +12,11 @@ app.set('view engine', 'ejs');
 app.set('view options', { delimiter: '?' });
 app.use(verify(conf));
 
-app.get('/*', (req, res) => {
+app.get('/*', async (req, res) => {
+  const document = firestore.doc('test01/doc01');
+  const doc = await document.get();
   res.render('./index', {
-    msg: 'Welcome!'
+    msg: doc.get('msg')
   });
 });
 
